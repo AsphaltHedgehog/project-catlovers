0d80b8361084309d7c0b4a4601205ff3c0fa4728
AsphaltHedgehog <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-10-10 14:07:12 +0000

    Deploying to gh-pages from @ AsphaltHedgehog/project-catlovers@a6b6cea9cd2c9025855646f8a82be75b9ea72d94 🚀

---
+++ index.16bd59bb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".support-arrow-container"),o=document.querySelector(".support-list");let t=!0;e.addEventListener("click",(r=>{r.preventDefault(),t?(t=!t,o.scrollIntoView({block:"end",behavior:"smooth"}),e.classList.add("up")):(t=!0,o.scrollIntoView({block:"start",behavior:"smooth"}),e.classList.remove("up"))}));
+//# sourceMappingURL=index.16bd59bb.js.map
---
+++ index.21a6c1b7.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".support-arrow-container"),e=document.querySelector(".support-list"),t=!0;o.addEventListener("click",(function(r){r.preventDefault(),t?(t=!t,e.scrollIntoView({block:"end",behavior:"smooth"}),o.classList.add("up")):(t=!0,e.scrollIntoView({block:"start",behavior:"smooth"}),o.classList.remove("up"))}))}();
+//# sourceMappingURL=index.21a6c1b7.js.map
