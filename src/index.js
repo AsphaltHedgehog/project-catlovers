da04b9c899bf5d1d5007cc9af788ca3d954ebdab
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-10-11 21:45:17 +0300

    bagfix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-<<<<<<< Updated upstream
 import './js/main.js';
-import './js/categories'
+import './js/categories';
 import './js/support-section.js';
 import './js/header-current-link.js';
 import './js/mobile-modal-window';
@@ -8,10 +7,8 @@ import './js/pop-up-modal-window.js';
 import './js/pop-up-modal-rendering.js';
 import './js/modal-current-link';
 
-=======
 import './js/categories';
 import './js/support-section.js';
 import './js/header-current-link.js';
 import './js/mobile-modal-window';
 import './js/loader.js';
->>>>>>> Stashed changes
