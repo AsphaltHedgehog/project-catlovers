f8cc4ca5d4219a6ee54e363f13ae8ae6ad5e6793
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-10-11 21:24:13 +0300

    add loader(test)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './js/main.js';
 import './js/categories'
 import './js/support-section.js';
@@ -7,3 +8,10 @@ import './js/pop-up-modal-window.js';
 import './js/pop-up-modal-rendering.js';
 import './js/modal-current-link';
 
+=======
+import './js/categories';
+import './js/support-section.js';
+import './js/header-current-link.js';
+import './js/mobile-modal-window';
+import './js/loader.js';
+>>>>>>> Stashed changes
