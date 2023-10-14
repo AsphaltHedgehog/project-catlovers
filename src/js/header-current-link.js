50f024359c70c91696edcc30be5e445c0525c5d1
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-14 16:46:35 +0300

    update header color

--- src/js/header-current-link.js
+++ src/js/header-current-link.js
@@ -1,12 +1,15 @@
 function addCurentLinkNewClass() {
     
     const linkEl = document.querySelectorAll('.link-nav');
+    const svgEl = document.querySelector('.icon-nav')
     let path = window.location.pathname; 
+    console.log(svgEl);
     
     linkEl.forEach(function (link) {
         let linkPath = link.getAttribute('href');
         if (path === linkPath) {
             link.classList.add('current');
+            svgEl.classList.add('svg-current')
         }
     })
 }
